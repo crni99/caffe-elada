c5d4c8585ecdbf5b9dd1e469bc9af659e32c08b3
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2025-11-27 01:03:45 +0100

    fix mobile nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,60 +1,57 @@
-import React, { useEffect, useState } from 'react';
-import { useTranslation } from 'react-i18next';
-import { Link, useLocation } from 'react-router-dom';
-import { NavDropdown } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons';
-import { useLanguage } from '../context/LanguageContext';
+import React, { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { Link, useLocation } from "react-router-dom";
+import { NavDropdown } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faXmark, faBars } from "@fortawesome/free-solid-svg-icons";
+import { useLanguage } from "../context/LanguageContext";
+
 import flagSr from "../assets/lang-flags/sr.svg";
 import flagEn from "../assets/lang-flags/en.svg";
 import flagGr from "../assets/lang-flags/gr.svg";
 
 export default function Header() {
-
     const { t } = useTranslation();
     const { language, changeLanguage } = useLanguage();
     const location = useLocation();
-    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    const toggleMobileMenu = () => {
-        setMobileMenuOpen(!mobileMenuOpen);
-    };
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const toggleMobileMenu = () => setMobileMenuOpen((prev) => !prev);
 
     useEffect(() => {
-        if (mobileMenuOpen) {
-            document.body.classList.add('mobile-nav-active');
-        } else {
-            document.body.classList.remove('mobile-nav-active');
+        if (location.hash) {
+            const el = document.querySelector(location.hash);
+            if (el) {
+                setTimeout(() => {
+                    el.scrollIntoView({ behavior: "smooth" });
+                }, 50);
+            }
         }
-    }, [mobileMenuOpen]);
-
-    const handleLanguageChange = (newLanguage) => {
-        changeLanguage(newLanguage);
-    };
+    }, [location]);
 
     const getLinkClass = (path) => {
-        if (path.startsWith('/#')) {
-            const hash = path.substring(1);
-            if (location.pathname === '/' && location.hash === hash) {
-                return 'active';
-            }
-        } else {
-            if (location.pathname === path) {
-                return 'active';
-            }
+        if (path.includes("#")) {
+            const [base, hash] = path.split("#");
+            const currentHash = location.hash.replace("#", "");
+            if (location.pathname === base && currentHash === hash) return "active";
         }
-        return '';
+        if (location.pathname === path) return "active";
+        return "";
     };
 
     return (
-        <header id="header" className="header d-flex align-items-center sticky-top">
-            <div className="container position-relative d-flex align-items-center justify-content-between">
-                <Link to="/" className="logo d-flex align-items-center me-auto me-xl-0">
-                    <img src="/assets/images/EladaLogoV2.svg" alt="Elada Logo" />
+        <header id="header" className="custom-header sticky-top">
+            <div className="container d-flex align-items-center justify-content-between">
+                <Link to="/" className="logo">
+                    <img src="/assets/images/EladaLogo.svg" alt="Elada Logo" />
                 </Link>
-
-                <nav id="navmenu" className="navmenu">
-                    <ul>
+                <FontAwesomeIcon
+                    icon={mobileMenuOpen ? faXmark : faBars}
+                    className="custom-mobile-toggle d-lg-none"
+                    onClick={toggleMobileMenu}
+                />
+                <nav className={`custom-navmenu ${mobileMenuOpen ? "open" : ""}`}>
+                    <ul className="main-links">
                         <li>
                             <Link to="/" className={getLinkClass("/")} onClick={() => setMobileMenuOpen(false)}>
                                 {t("Home")}
@@ -75,38 +72,33 @@ export default function Header() {
                                 {t("Drinks.title")}
                             </Link>
                         </li>
-                        <li>
-                            <NavDropdown
-                                title={
-                                    <img
-                                        src={language === 'sr' ? flagSr : language === 'gr' ? flagGr : flagEn}
-                                        alt="Language Flag"
-                                        width="20"
-                                        height="14"
-                                    />
-                                }
-                                id="language-dropdown-header-btn"
-                                align="end"
-                                className="btn-getstarted"
-                            >
-                                <NavDropdown.Item onClick={() => handleLanguageChange('sr')}>
-                                    &nbsp;<img src={flagSr} alt="Serbian" width="20" height="14" /> &nbsp;
-                                </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => handleLanguageChange('gr')}>
-                                    &nbsp;<img src={flagGr} alt="Greek" width="20" height="14" /> &nbsp;
-                                </NavDropdown.Item>
-                                <NavDropdown.Item onClick={() => handleLanguageChange('en')}>
-                                    &nbsp;<img src={flagEn} alt="English" width="20" height="14" /> &nbsp;
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </li>
                     </ul>
+                    <div className="language-container">
+                        <NavDropdown
+                            title={
+                                <img
+                                    src={language === "sr" ? flagSr : language === "gr" ? flagGr : flagEn}
+                                    width="20"
+                                    height="14"
+                                    alt="Language"
+                                />
+                            }
+                            id="language-dropdown"
+                            align="end"
+                            className="language-dropdown"
+                        >
+                            <NavDropdown.Item onClick={() => changeLanguage("sr")}>
+                                <img src={flagSr} width="20" height="14" alt="SR" />
+                            </NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => changeLanguage("gr")}>
+                                <img src={flagGr} width="20" height="14" alt="GR" />
+                            </NavDropdown.Item>
+                            <NavDropdown.Item onClick={() => changeLanguage("en")}>
+                                <img src={flagEn} width="20" height="14" alt="EN" />
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </div>
                 </nav>
-                <FontAwesomeIcon
-                    icon={mobileMenuOpen ? faXmark : faBars}
-                    className="mobile-nav-toggle d-xl-none"
-                    onClick={toggleMobileMenu}
-                />
             </div>
         </header>
     );
