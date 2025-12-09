000697eca319c55ce0f37b4575b234959412eb8c
Ognjen Andjelic <andjelicb.ognjen@gmail.com>
2025-12-09 17:03:26 +0100

    update preloader to accept className

--- src/components/Preloader.jsx
+++ src/components/Preloader.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Preloader = ({ logoSrc }) => {
+const Preloader = ({ logoSrc, className }) => {
     return (
-        <div className='preloader-overlay'>
+        <div className={`preloader-overlay ${className}`}>
             <img src={logoSrc} alt="Loading Logo" className="preloader-logo" />
         </div>
     );
